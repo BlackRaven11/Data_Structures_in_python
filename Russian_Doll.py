11fe0efe91bb0dd5ee1f85f88c69650a24d2b64f
giso maleki mansour <blackcorvum@gmail.com>
2023-04-23 23:33:19 +0430

    edit

--- Russian_Doll.py
+++ Russian_Doll.py
@@ -1,9 +1,9 @@
-def open_Russian_Doll(doll):
+def open_russian_doll(doll):
   if doll == 1:
     print('doll = 1 --> All Dolls are opened!')
   else:
-      print('doll = ' , doll)
-      open_Russian_Doll(doll-1)
+    print('doll = ' , doll)
+    open_russian_doll(doll-1)
       
-open_Russian_Doll(4)
+open_russian_doll(4)
         
